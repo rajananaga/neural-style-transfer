23f3096fb54bb6b6a3a1bdcb257304de6d6c236a
Ankit Mathur <ankmathur96@gmail.com>
2017-12-03 07:57:15 +0000

    small change

--- neural.py
+++ neural.py
@@ -130,7 +130,7 @@ def construct_image(content, style):
                     cloned_param = target.clone()
                 print(cloned_param.data.size())
                 im = cloned_param.squeeze(0).data
-                denorm = transforms.Normalize((-2.12, -2.04, -1.80), (4.37, 4.46, 4.44))
+                denorm = trans.Normalize((-2.12, -2.04, -1.80), (4.37, 4.46, 4.44))
                 utils.save_image(denorm(im).clamp(0,1), OUT_PATH + 'output_' + str(i) + '.'+ F_EXT)
             return loss
         optimizer.step(closure)
